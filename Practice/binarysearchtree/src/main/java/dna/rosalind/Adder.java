c768d60c369b86dd5b760d6f52f0697d055f4fd6
Gabe Clark <unrivalled.dh@gmail.com>
2017-06-29 11:51:37 -0700

    Begin working on Adder

--- Practice/binarysearchtree/src/main/java/dna/rosalind/Adder.java
+++ Practice/binarysearchtree/src/main/java/dna/rosalind/Adder.java
@@ -4,4 +4,17 @@ package dna.rosalind;
  * Created by gabe.clark on 6/29/17.
  */
 public class Adder {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public String add(String a, String b){
+        if(a == null || b == null)
+            return null;
+
+        StringBuilder builder = new StringBuilder();
+        return builder.append(a).append(b).toString();
+    }
+
 }
