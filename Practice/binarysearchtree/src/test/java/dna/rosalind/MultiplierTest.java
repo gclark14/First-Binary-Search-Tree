eff762cc33ea4e2511c9098a1aff817ad5d28e9d
Gabe Clark <unrivalled.dh@gmail.com>
2017-06-30 10:04:27 -0700

    Added test for multiplication of three real numbers

--- Practice/binarysearchtree/src/test/java/dna/rosalind/MultiplierTest.java
+++ Practice/binarysearchtree/src/test/java/dna/rosalind/MultiplierTest.java
@@ -20,4 +20,9 @@ public class MultiplierTest {
     public void multiply_should_return_product_given_two_doubles(){
         assertThat(multiplier.multiply(1.5,2.0), is(3.0));
     }
+
+    @Test
+    public void multiply_should_return_product_given_three_real_numbers(){
+        assertThat(multiplier.multiply(1.0,1.0,2.0), is(2.0));
+    }
 }
