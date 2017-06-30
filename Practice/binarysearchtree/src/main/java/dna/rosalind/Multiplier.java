29fa232026f1bc98eecfe309b97ba9aa1598b296
Gabe Clark <unrivalled.dh@gmail.com>
2017-06-30 09:43:23 -0700

    Can now multiply integer with double

--- Practice/binarysearchtree/src/main/java/dna/rosalind/Multiplier.java
+++ Practice/binarysearchtree/src/main/java/dna/rosalind/Multiplier.java
@@ -4,8 +4,11 @@ package dna.rosalind;
  * Created by gabe.clark on 6/29/17.
  */
 public class Multiplier {
+
     int multiply(int a, int b){ return a * b; }
 
     double multiply(double d, double b) { return d * b; }
+
+    double multiply(int a, double d) { return a * d; }
 
 }
