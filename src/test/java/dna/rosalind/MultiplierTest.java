bb9fd6fdc1eede78351b0a612f6990a4926ef1d8
Gabe Clark <unrivalled.dh@gmail.com>
2017-07-06 11:37:15 -0700

    Decreased coverage

--- src/test/java/dna/rosalind/MultiplierTest.java
+++ src/test/java/dna/rosalind/MultiplierTest.java
@@ -15,7 +15,7 @@ public class MultiplierTest {
     public void multiply_should_return_product_given_two_integers(){
         assertThat(multiplier.multiply(2,2), is(4));
     }
-
+    /*
     @Test
     public void multiply_should_return_product_given_two_doubles(){
         assertThat(multiplier.multiply(1.5,2.0), is(3.0));
@@ -24,6 +24,6 @@ public class MultiplierTest {
     @Test
     public void multiply_should_return_product_given_three_real_numbers(){
         assertThat(multiplier.multiply(1.0,1.0,2.0), is(2.0));
-    }
+    }*/
 
 }
